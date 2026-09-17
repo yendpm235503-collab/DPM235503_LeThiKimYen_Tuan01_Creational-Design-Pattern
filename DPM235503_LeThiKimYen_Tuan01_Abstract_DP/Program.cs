a2e8a93b0c00458068eadd3caba5d39cc344ba0d
Le Thi Kim Yen <yen_dpm235503@student.agu.edu.vn>
2026-09-17 14:10:56 +0700

    Update Program.cs

--- DPM235503_LeThiKimYen_Tuan01_Abstract_DP/Program.cs
+++ DPM235503_LeThiKimYen_Tuan01_Abstract_DP/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace DPM235503_LeThiKimYen_Tuan01_Abstract_DP
 {
     public interface IAbstractProductA { string UsefulFunctionA(); }
-    public class ConcreteProductA1 : IAbstractProductA { public string UsefulFunctionA() => "Kết quả từ Product A1"; }
-    public class ConcreteProductA2 : IAbstractProductA { public string UsefulFunctionA() => "Kết quả từ Product A2"; }
+    public class ConcreteProductA1 : IAbstractProductA { public string UsefulFunctionA() => "Kết quả Sản phẩm A1"; }
+    public class ConcreteProductA2 : IAbstractProductA { public string UsefulFunctionA() => "Kết quả Sản phẩm A2"; }
 
     public interface IAbstractProductB { string UsefulFunctionB(); }
-    public class ConcreteProductB1 : IAbstractProductB { public string UsefulFunctionB() => "Kết quả từ Product B1"; }
-    public class ConcreteProductB2 : IAbstractProductB { public string UsefulFunctionB() => "Kết quả từ Product B2"; }
+    public class ConcreteProductB1 : IAbstractProductB { public string UsefulFunctionB() => "Kết quả Sản phẩm B1"; }
+    public class ConcreteProductB2 : IAbstractProductB { public string UsefulFunctionB() => "Kết quả Sản phẩm B2"; }
 
     public interface IAbstractFactory
     {
@@ -34,10 +34,10 @@ namespace DPM235503_LeThiKimYen_Tuan01_Abstract_DP
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("--- ABSTRACT FACTORY PATTERN (LÝ THUYẾT) ---");
-            IAbstractFactory factory1 = new ConcreteFactory1();
-            Console.WriteLine(factory1.CreateProductA().UsefulFunctionA());
-            Console.WriteLine(factory1.CreateProductB().UsefulFunctionB());
+            Console.WriteLine("=== ABSTRACT FACTORY PATTERN (LÝ THUYẾT) ===");
+            IAbstractFactory factory = new ConcreteFactory1();
+            Console.WriteLine(factory.CreateProductA().UsefulFunctionA());
+            Console.WriteLine(factory.CreateProductB().UsefulFunctionB());
             Console.ReadKey();
         }
     }
