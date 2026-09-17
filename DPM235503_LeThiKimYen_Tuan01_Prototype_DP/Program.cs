e684bfec41bb003e9f3593c1e7032ca4294085d0
Le Thi Kim Yen <yen_dpm235503@student.agu.edu.vn>
2026-09-17 14:15:11 +0700

    Update Program.cs

--- DPM235503_LeThiKimYen_Tuan01_Prototype_DP/Program.cs
+++ DPM235503_LeThiKimYen_Tuan01_Prototype_DP/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace DPM235503_LeThiKimYen_Tuan01_Prototype_DP
 {
-    public class Person
+    public class ConcretePrototype
     {
-        public int Age { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
 
-        public Person ShallowCopy() => (Person)this.MemberwiseClone();
+        public ConcretePrototype Clone() => (ConcretePrototype)this.MemberwiseClone();
     }
 
     class Program
@@ -16,12 +16,12 @@ namespace DPM235503_LeThiKimYen_Tuan01_Prototype_DP
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("--- PROTOTYPE PATTERN (LÝ THUYẾT) ---");
-            Person p1 = new Person { Age = 20, Name = "An Giang" };
-            Person p2 = p1.ShallowCopy();
+            Console.WriteLine("=== PROTOTYPE PATTERN (LÝ THUYẾT) ===");
+            ConcretePrototype p1 = new ConcretePrototype { Id = 1, Name = "Mẫu gốc" };
+            ConcretePrototype p2 = p1.Clone();
 
-            Console.WriteLine($"P1: {p1.Name}, {p1.Age}");
-            Console.WriteLine($"P2 (Sao chép): {p2.Name}, {p2.Age}");
+            Console.WriteLine($"P1: {p1.Id} - {p1.Name}");
+            Console.WriteLine($"P2 (Clone): {p2.Id} - {p2.Name}");
             Console.ReadKey();
         }
     }
